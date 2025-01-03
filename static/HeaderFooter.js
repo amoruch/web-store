886bdcee5909bfb0395b796759e63f7f6f1f3bdb
amoruch <roma.usha@yandex.ru>
2025-01-03 14:16:10 +0300

    kinda global changes
    changed header and footer using js
    new profile page

---
+++ static/HeaderFooter.js
@@ -0,0 +1,45 @@
+class MyHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <a class="logo" href="/templates/home.html">World Peas</a>
+            <nav class="navigation">
+                <ul>
+                    <li class="li-shop"><a href="/templates/shop.html">Shop</a></li>
+                    <li class="li-who"><a href="/templates/home.html">Who we are</a></li>
+                    <li class="li-prof"><a href="/templates/sign-in.html">My profile</a></li>
+                    <li class="li-basket"><a href="/templates/basket.html">Basket (3)</a></li>
+                </ul>
+            </nav>
+        `
+    }
+}
+
+class MyFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="copyright">
+                <a class="logo" href="/templates/home.html">World Peas</a>
+                <p>Copyright © 2024 World Peas, Inc.</p>
+            </div>
+            <div class="contacts">
+                <p>CONTACT US</p>
+                <div class="contacts-imgs">
+                    <img src="/static/images/contacts/Twitter.png" alt="T">
+                    <img src="/static/images/contacts/Instagram.png" alt="I">
+                    <img src="/static/images/contacts/YouTube.png" alt="YT">
+                    <img src="/static/images/contacts/LinkedIn.png" alt="LI">
+                </div>
+            </div>
+            <div class="subscribe">
+                <p>Subscribe for freshest news</p>
+                <form action="">
+                    <input type="text" placeholder="you@example.com">
+                    <button class="active">Subscribe</button>
+                </form>
+            </div>
+        `
+    }
+}
+
+customElements.define('my-header', MyHeader)
+customElements.define('my-footer', MyFooter)
--- static/shop.js
+++ static/shop.js
@@ -3,7 +3,7 @@ shop = document.getElementsByClassName("shop")[0]
 
 let product_count = 2;
 
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 2; i++) {
     let product = product_template.content.cloneNode(true);
 
     product.querySelector("img").src += (i % product_count + 1) + ".png";
